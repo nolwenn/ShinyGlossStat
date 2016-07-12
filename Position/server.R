1521a5059b874dfd8cc5ea4398c4afccfb691422
nlemeur <nlemeur@gmail.com>
2016-07-12 22:57:39 +0200

    improve definition and visual dispay

--- Position/server.R
+++ Position/server.R
@@ -1,19 +1,33 @@
 library(MASS)
+library(ggplot2)
 data(anorexia)
 
+
+
 shinyServer(function(input, output) {
+  anorexia$Prewt <- round(anorexia$Prewt,0)
   
   output$plot<-renderPlot({
-  ggplot(anorexia[1:input$n,], aes(x = 1:length(Prewt), y = Prewt)) +
+    ds <-c("mean"=mean(anorexia$Prewt), "median"=median(anorexia$Prewt))
+    ggplot(anorexia[1:input$n,], aes(x = 1:length(Prewt), y = Prewt)) +
     labs(title = "", x = "Individuals", y = "Weight (lbs)") +
-    geom_point() 
-    ##geom_point(data = ds, aes(y = mean),
-    ##           colour = 'red', size = 3)
+    geom_point() + 
+    geom_hline(aes(yintercept=mean(anorexia$Prewt[1:input$n]), color="mean")) +
+    geom_hline(aes(yintercept=median(anorexia$Prewt[1:input$n]), color="median"))
+    #geom_smooth(method=lm,   # Add linear regression lines
+    #              se=FALSE,    # Don't add shaded confidence region
+    #              fullrange=TRUE)
   })
   
   output$summary <- renderPrint({
-    x = anorexia$Prewt[1:input$n]
-    c("Mean"= mean(x), "Median"= median(x))
+    x = round(anorexia$Prewt[1:input$n],0)
+    getmode <- function(v) {
+      w = table(v)
+      m= w[max(w)==w]
+      if(m==1) {return(NA)}
+      else{return(m)}
+    }
+    c("Mean"= mean(x), "Median"= median(x), "Mode"=getmode(x))
   })
   
 })
--- Position/ui.R
+++ Position/ui.R
@@ -18,16 +18,18 @@ shinyUI(fluidPage(
       tabsetPanel(
         tabPanel("Definition", 
           h3("Mean", align = "left"),
-          p("The mean of a data series is the sum of the values divided 
-          by the number of values in the data series."),
-          code("mean(x, na.rm=T)"),
+          p("The mean of a data series is the sum of the values divided by the number of values in the data series."),
+          p("Example 1: if a child eats 2, 4, 5, 3, 3, 3, and 4 cookies per day over a week, he eats on average (2+4+5+3+3+3+4)/7 = 3.4 per day"),
+          code("mean(x)"),
           h3("Median", align = "left"),
           p("The median is the middle most value in a data series: 
           50% of the values are lower than the median and 50% are higher than the median."),
+          p("Example 1: if a child eats 2, 4, 5, 3, 3, 3, and 4 cookies per day over a week, his median is 3 (value of the middle after increasing sorting). In other words, 50% of the days of the week he eats 3 or less than 3 cookies and the other days he eats 3 or more."),
           code("median(x)"),
           h3("Mode", align = "left"),
           p("The mode is the value that has the highest number of occurrences in a set of data. 
           Unike mean and median, mode can have both numeric and character data."),
+          p("Example 1: if a child eats 2, 4, 5, 3, 3, 3, and 4 cookies per day over a week, he most often eats 3 cookies; the mode is 3."),
           code("mode(x)")
         ),
         
